9c9b767ad00e589aa3126d2b04559b8710dc54c6
Yang,Zhou <thomaszdxsn@icloud.com>
2022-03-14 12:45:10 +0800

    :sparkles: add only one restrict for representation in OrgCard

--- src/components/MemberFieldSet.tsx
+++ src/components/MemberFieldSet.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useWatch } from "react-hook-form";
 import { useFormContext } from "../models";
+import type { FormValues } from "../interfaces";
 import DragHandle from "./DragHandle";
 
 interface FieldSetProps {
@@ -12,18 +13,72 @@ interface PresentationCheckboxProps {
   name: `${FieldSetProps["name"]}`;
 }
 
+function getMembersNamePath(name: FieldSetProps["name"]) {
+  const path = name.split(".");
+  return path.slice(0, path.length - 1).join(".") as `orgs.${number}.members`;
+}
+
+function getMembersFromFormValues(
+  formValues: FormValues,
+  pathStr: ReturnType<typeof getMembersNamePath>
+) {
+  const [orgField, orgIndexField, membersField] = pathStr.split(".") as [
+    `orgs`,
+    `${number}`,
+    `members`
+  ];
+  const indexField = Number(orgIndexField);
+  return formValues[orgField][indexField][membersField];
+}
+
 const RepresentationCheckbox: React.FC<PresentationCheckboxProps> = ({
   name,
 }) => {
-  const { register, control } = useFormContext();
+  const { register, control, setValue, getValues } = useFormContext();
   const activated = useWatch({ control, name: `${name}.activated` });
   const disabled = React.useMemo(() => !activated, [activated]);
+  const representation = useWatch({ control, name: `${name}.representation` });
+  const oldRepresentation = React.useRef(representation);
+  const fieldName = `${name}.representation` as const;
+  React.useEffect(() => {
+    if (representation !== oldRepresentation.current) {
+      console.log({ new: representation, name });
+      oldRepresentation.current = representation;
+
+			// reselect other members representation when current
+      if (representation === true) {
+        const membersNamePath = getMembersNamePath(name);
+        const membersValue = getMembersFromFormValues(
+          getValues(),
+          membersNamePath
+        );
+        console.log({ membersValue, membersNamePath, values: getValues() });
+        membersValue.forEach((member, index) => {
+          const representationPath =
+            `${membersNamePath}.${index}.representation` as const;
+          if (
+            representationPath !== fieldName &&
+            member.representation === true
+          ) {
+            setValue(representationPath, false);
+          }
+        });
+        console.log({ values: getValues() });
+      }
+    }
+  }, [representation, fieldName]);
+  React.useEffect(() => {
+    // reselect representation when disabled
+    if (disabled) {
+      setValue(fieldName, false);
+    }
+  }, [disabled, fieldName]);
   return (
     <input
       type="checkbox"
       className="disabled:opacity-50 disabled:cursor-not-allowed"
       disabled={disabled}
-      {...register(`${name}.representation`)}
+      {...register(fieldName)}
     />
   );
 };
